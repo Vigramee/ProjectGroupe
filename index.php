a26907596ddbdc57d5899308b5ecaa3b567a2822
Romain CROS <romaincros17@gmail.com>
2025-11-21 13:51:26 +0100

    update

--- index.php
+++ index.php
@@ -1,14 +1,4 @@
 <?php
-try {
-    $dbh = new PDO(
-        'mysql:host=localhost;dbname=airbnb;charset=utf8',
-        'root',
-        '13062007'
-    );
-} catch (PDOException $e){
-    die($e->getMessage());
-}
-
 
 if (isset($_POST['add'])) {
 
